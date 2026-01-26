95b99b78ea68a3a4166ba1295d5015e9f0c8a0eb
Auri <auritro.sami@gmail.com>
2026-01-27 10:41:23 +1100

    fix: used port 8001 for 2-brain-api to make sure both backends run

--- ML_side/main.py
+++ ML_side/main.py
@@ -182,4 +182,4 @@ async def two_brain_endpoint(file: UploadFile = File(...), question: str = Form(
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8001)
